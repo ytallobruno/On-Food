ce54ba5100c41b230fc49c2415b31ee3ee585d24
Ytallo Bruno <ytallobruno@hotmail.com>
2022-04-15 13:45:45 -0300

    feat: add icons svg import and export

---
+++ icons/index.ts
@@ -0,0 +1,19 @@
+import IconBookmark from "../public/images/icons/bookmark.svg";
+import IconCategory from "../public/images/icons/category.svg";
+import IconDelivery from "../public/images/icons/delivery.svg";
+import IconFilter from "../public/images/icons/filter.svg";
+import IconHeart from "../public/images/icons/heart.svg";
+import IconHome from "../public/images/icons/home.svg";
+import IconRestaurant from "../public/images/icons/restaurant.svg";
+import IconReviews from "../public/images/icons/reviews.svg";
+
+export {
+  IconBookmark,
+  IconCategory,
+  IconDelivery,
+  IconFilter,
+  IconHeart,
+  IconHome,
+  IconRestaurant,
+  IconReviews,
+};
