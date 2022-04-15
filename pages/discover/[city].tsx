25521f11681f940ae597db36451d9809e7656823
Ytallo Bruno <ytallobruno@hotmail.com>
2022-04-15 13:37:22 -0300

    feat: add pages discover and favorites

---
+++ pages/discover/[city].tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+
+export default function Descobrir() {
+  const router = useRouter();
+  const city = router.query.city;
+  return <h1>Descobrir, {city}</h1>;
+}
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,7 @@
+export default function Favoritos() {
+  return (
+    <div>
+      <h1>Favoritos</h1>
+    </div>
+  );
+}
