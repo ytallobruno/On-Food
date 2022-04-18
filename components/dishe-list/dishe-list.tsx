8bc25fb81407d0779b77e34e027a60820cc59b4a
Ytallo Bruno <ytallobruno@hotmail.com>
2022-04-18 18:45:04 -0300

    feat: create dishe-list component

---
+++ components/dishe-list/dishe-list.tsx
@@ -0,0 +1,13 @@
+import Styles from "./dishe-list.module.css";
+import { Dishe } from "@/components";
+
+export function DisheList() {
+  return (
+    <div className={Styles.list}>
+      <Dishe />
+      <Dishe />
+      <Dishe />
+      <Dishe />
+      </div>
+  );
+}
